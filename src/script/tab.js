bbbf26ae88cad76ff188d5387089569b0c6a0db4
seed-fe <zzlfullstack@gmail.com>
2017-04-04 18:51:52 +0800

    实现了自动切换的功能
    * 页面加载时让第一个tab显示
    * 用间歇调用让索引值自增，然后显示当前索引值的内容
    * 索引值达到最大时要清零

--- src/script/tab.js
+++ src/script/tab.js
@@ -2,89 +2,117 @@ function $ (id) {
 	// body... 
 	return typeof id === 'string' ? document.getElementById(id) : id;
 }
-window.onload = function () {
-	/* body... */
-	// tab索引和定时器
-	var index = 0,
+// window.onload = function () {
+// 	/* body... */
+// 	// tab索引和定时器
+// 	var index = 0,
+// 		timer = null;
+// 	// 获取鼠标滑过或点击的标签和要切换内容的元素
+// 	var tabs = $('notice-title').getElementsByTagName('li'),
+// 		contents = $('notice-content').getElementsByTagName('div');
+// 	if (tabs.length != contents.length) {
+// 		return;
+// 	}
+// 	/*用自定义属性方式存储索引*/
+// 	// 遍历tabs
+// 	// for (var i = 0; i < tabs.length; i++) {
+// 	// 	// 给tab增加一个自定义属性number用来保存索引
+// 	// 	tabs[i].number = i;
+// 	// 	// 鼠标滑过切换
+// 	// 	// tabs[i].onmouseover = function (argument) {
+// 	// 	// 	/* body... */
+// 	// 	// 	// 清除所有li上的class
+// 	// 	// 	for (var j = 0; j < tabs.length; j++) {
+// 	// 	// 		tabs[j].className = '';
+// 	// 	// 		contents[j].style.display = 'none';
+// 	// 	// 	}
+// 	// 	// 	// 设置当前为高亮显示
+// 	// 	// 	this.className = 'selected';
+// 	// 	// 	contents[this.number].style.display = 'block';
+// 	// 	// }
+// 	// 	// 鼠标点击切换
+// 	// 	tabs[i].onclick = function (argument) {
+// 	// 		/* body... */
+// 	// 		// 清除所有li上的class
+// 	// 		for (var j = 0; j < tabs.length; j++) {
+// 	// 			tabs[j].className = '';
+// 	// 			// 通过className来操作内容的显示切换，避免内联样式代码
+// 	// 			contents[j].className = 'mod';
+// 	// 		}
+// 	// 		// 设置当前为高亮显示
+// 	// 		this.className = 'selected';
+// 	// 		contents[this.number].className = 'mod mod-current';
+// 	// 	}
+// 	// }
+// 	用立即执行匿名函数和闭包的方式存储索引
+// 	/*for (var i = 0; i < tabs.length; i++) {
+// 		(function (e) {
+// 			tabs[e].onclick = function() {
+// 				// 清除所有li上的class
+// 				for (var j = 0; j < tabs.length; j++) {
+// 					tabs[j].className = '';
+// 					// 通过className来操作内容的显示切换，避免内联样式代码
+// 					contents[j].className = 'mod';
+// 				}
+// 				// 设置当前为高亮显示
+// 				this.className = 'selected';
+// 				contents[e].className = 'mod mod-current';
+// 			}
+// 		})(i);
+// 	}*/
+// 	/*延迟切换用mouseover*/
+// 	/*for (var i = 0; i < tabs.length; i++) {
+// 		(function (e) {
+// 			tabs[e].onmouseover = function() {
+// 				// setTimeout时window对象的方法，里面的函数执行环境就是window，因此this对象就是window，所以这里要将当前this对象（鼠标滑过的li）取出来
+// 				var that = this;
+// 				// 如果存在准备执行的定时器，立刻清除，只有在当前选项卡上停留时间大于500ms时才执行tab切换，因为给定时器的延迟时间就是500ms
+// 				if (timer) {
+// 					clearTimeout(timer);
+// 					console.log(timer);
+// 					timer = null;
+// 					console.log(timer);
+// 				}
+// 				timer = setTimeout(function() {
+// 					// 清除所有li上的class
+// 					for (var j = 0; j < tabs.length; j++) {
+// 						tabs[j].className = '';
+// 						// 通过className来操作内容的显示切换，避免内联样式代码
+// 						contents[j].className = 'mod';
+// 					}
+// 					// 设置当前为高亮显示
+// 					console.log(that);
+// 					that.className = 'selected';
+// 					contents[e].className = 'mod mod-current';
+// 				}, 500);
+// 			}
+// 		})(i);
+// 	}*/
+// }
+/*自动切换*/
+window.onload = tab;
+function tab () {
+	// body...
+	var index = 0, //当前显示的tab的索引，初值是0，表示一开始显示第一个
 		timer = null;
-	// 获取鼠标滑过或点击的标签和要切换内容的元素
 	var tabs = $('notice-title').getElementsByTagName('li'),
 		contents = $('notice-content').getElementsByTagName('div');
 	if (tabs.length != contents.length) {
 		return;
 	}
-	// 遍历tabs
-	// for (var i = 0; i < tabs.length; i++) {
-	// 	// 给tab增加一个自定义属性number用来保存索引
-	// 	tabs[i].number = i;
-	// 	// 鼠标滑过切换
-	// 	// tabs[i].onmouseover = function (argument) {
-	// 	// 	/* body... */
-	// 	// 	// 清除所有li上的class
-	// 	// 	for (var j = 0; j < tabs.length; j++) {
-	// 	// 		tabs[j].className = '';
-	// 	// 		contents[j].style.display = 'none';
-	// 	// 	}
-	// 	// 	// 设置当前为高亮显示
-	// 	// 	this.className = 'selected';
-	// 	// 	contents[this.number].style.display = 'block';
-	// 	// }
-	// 	// 鼠标点击切换
-	// 	tabs[i].onclick = function (argument) {
-	// 		/* body... */
-	// 		// 清除所有li上的class
-	// 		for (var j = 0; j < tabs.length; j++) {
-	// 			tabs[j].className = '';
-	// 			// 通过className来操作内容的显示切换，避免内联样式代码
-	// 			contents[j].className = 'mod';
-	// 		}
-	// 		// 设置当前为高亮显示
-	// 		this.className = 'selected';
-	// 		contents[this.number].className = 'mod mod-current';
-	// 	}
-	// }
-	/*用立即执行匿名函数和闭包的方式存储索引*/
-	/*for (var i = 0; i < tabs.length; i++) {
-		(function (e) {
-			tabs[e].onclick = function() {
-				// 清除所有li上的class
-				for (var j = 0; j < tabs.length; j++) {
-					tabs[j].className = '';
-					// 通过className来操作内容的显示切换，避免内联样式代码
-					contents[j].className = 'mod';
-				}
-				// 设置当前为高亮显示
-				this.className = 'selected';
-				contents[e].className = 'mod mod-current';
-			}
-		})(i);
-	}*/
-	/*延迟切换用mouseover*/
-	for (var i = 0; i < tabs.length; i++) {
-		(function (e) {
-			tabs[e].onmouseover = function() {
-				// setTimeout时window对象的方法，里面的函数执行环境就是window，因此this对象就是window，所以这里要将当前this对象（鼠标滑过的li）取出来
-				var that = this;
-				// 如果存在准备执行的定时器，立刻清除，只有在当前选项卡上停留时间大于500ms时才执行tab切换，因为给定时器的延迟时间就是500ms
-				if (timer) {
-					clearTimeout(timer);
-					console.log(timer);
-					timer = null;
-					console.log(timer);
-				}
-				timer = setTimeout(function() {
-					// 清除所有li上的class
-					for (var j = 0; j < tabs.length; j++) {
-						tabs[j].className = '';
-						// 通过className来操作内容的显示切换，避免内联样式代码
-						contents[j].className = 'mod';
-					}
-					// 设置当前为高亮显示
-					console.log(that);
-					that.className = 'selected';
-					contents[e].className = 'mod mod-current';
-				}, 500);
-			}
-		})(i);
-	}
-}
+	// 添加间歇调用，改变当前高亮的索引
+	timer = setInterval(function() {
+		index++;
+		// 索引达到最大时要重新归零
+		if (index >= tabs.length) {
+			index = 0;
+		}
+		for (var j = 0; j < tabs.length; j++) {
+			tabs[j].className = '';
+			// 通过className来操作内容的显示切换，避免内联样式代码
+			contents[j].className = 'mod';
+		}
+		tabs[index].className = 'selected';
+		contents[index].className = 'mod mod-current';
+	}, 2000);
+}		
