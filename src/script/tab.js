60a9c18fb6bd61d5512a4261882c1a748a84502f
seed-fe <zzlfullstack@gmail.com>
2017-04-04 21:34:01 +0800

    在自动切换功能的基础上添加了手动切换的功能
    * 鼠标移入整个tab模块时清除定时器，解除自动切换
    * 鼠标移出整个tab模块时恢复自动切换
    * 鼠标移入某个tab选项时清除定时器，添加手动切换功能

--- src/script/tab.js
+++ src/script/tab.js
@@ -95,24 +95,55 @@ function tab () {
 	// body...
 	var index = 0, //当前显示的tab的索引，初值是0，表示一开始显示第一个
 		timer = null;
+	var tabDiv = $('notice');
 	var tabs = $('notice-title').getElementsByTagName('li'),
 		contents = $('notice-content').getElementsByTagName('div');
 	if (tabs.length != contents.length) {
 		return;
 	}
+	for (var i = 0; i < tabs.length; i++) {
+		(function (e) {
+			tabs[e].onmouseover = function() {
+				// 清除自动切换的定时器
+				clearInterval(timer);
+				// 清除所有li上的class
+				tabSwitch(e);
+			}
+		})(i);
+	}
+	// 鼠标移入整个tab模块时清除定时器，解除自动切换
+	tabDiv.onmouseover = function() {
+		clearInterval(timer);
+	}
+	// 鼠标移出整个tab模块时恢复自动切换
+	tabDiv.onmouseout = function() {
+		timer = setInterval(autoPlay, 2000);
+	}
+	if (timer) {
+		clearInterval(timer);
+		timer = null;
+	}
 	// 添加间歇调用，改变当前高亮的索引
-	timer = setInterval(function() {
+	timer = setInterval(autoPlay, 2000);
+	// 索引自增函数封装
+	function autoPlay(argument) {
+		// body...
 		index++;
 		// 索引达到最大时要重新归零
 		if (index >= tabs.length) {
 			index = 0;
 		}
+		tabSwitch(index);
+	}
+	// tab切换函数封装
+	function tabSwitch(curIndex) {
 		for (var j = 0; j < tabs.length; j++) {
 			tabs[j].className = '';
 			// 通过className来操作内容的显示切换，避免内联样式代码
 			contents[j].className = 'mod';
 		}
-		tabs[index].className = 'selected';
-		contents[index].className = 'mod mod-current';
-	}, 2000);
+		tabs[curIndex].className = 'selected';
+		contents[curIndex].className = 'mod mod-current';
+		index = curIndex;
+	}
 }		
