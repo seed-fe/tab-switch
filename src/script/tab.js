3ca4c550e540c71766d4abe0b767c4639c98ebb2
Bill Zhang <zzlfullstack@gmail.com>
2017-05-22 14:30:36 +0800

    添加了一些注释

--- src/script/tab.js
+++ src/script/tab.js
@@ -109,7 +109,7 @@
 	// 将一个对象添加到jquery的原型上从而提供新的jquery实例方法，这里就相当于给jquery实例对象提供了一个backtop方法，这也是实现jquery插件的常用方法，对象里定义的方法名就是插件方法名
 	$.fn.extend({
 	    tab: function(opts) {
-	        // return 是为了实现连缀，这里的this指代调用插件时用jquery选择的元素，调用each方法是因为可能会选择多个元素
+	        // return 是为了实现连缀，这里的this指代调用插件时用jquery选择的元素（转化成了一个jQuery对象的实例），调用each方法是因为可能会选择多个元素
 	        // .each()方法的回调函数的执行环境是当前dom元素，也就是说this始终指向当前dom元素，所以下面的回掉函数就是创建一个BackTop的实例，并把当前dom元素作为参数传入
 	        return this.each(function() {
 	            new Tab(this, opts); // 这里的this指向dom元素
